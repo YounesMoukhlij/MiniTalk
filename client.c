c5dfb5f9a6a5e8176a753d57c3e241a966b2d874
Younes Moukhlij <youmoukh@e2r3p5.1337.ma>
2023-12-21 21:37:12 +0100

    df

--- client.c
+++ client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: younesmoukhlij <younesmoukhlij@student.    +#+  +:+       +#+        */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:54:45 by youmoukh          #+#    #+#             */
-/*   Updated: 2023/12/19 01:36:17 by younesmoukh      ###   ########.fr       */
+/*   Updated: 2023/12/21 21:34:22 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 
 static void	show_mssg(int check)
 {
-	if (check == SIGUSR1 || check == SIGUSR2)
-		printf("Message sent\n");
-	else
-		printf("NOT SENT\n");
+	static int i;
+	
+	if (check == SIGUSR1)
+		++i;
+	else if (check == SIGUSR2)
+		i++;
 }
 
-static void	transmit_mssg(int pid, char c)
+static void	transmit_mssg(int pid, unsigned char c)
 {
 	int	i;
 
@@ -29,31 +31,38 @@ static void	transmit_mssg(int pid, char c)
 	while (i < 8)
 	{
 		if ((c & (0x01 << i)) != 0)
-			kill(pid, SIGUSR1);
-		else
 			kill(pid, SIGUSR2);
-		usleep(100);
+		else if ((c & (0x01 << i)) == 0)
+			kill(pid, SIGUSR1);
 		i++;
 	}
+	if (i == 8)
+		{
+				usleep(100);
+		}
+	
+		
 }
 
 int	main(int ac, char *av[])
 {
 	int pid;
 	int	i;
-
-	pid = atoi(av[1]);
+	int index = 0 ;
 	i = 0x0;
 	if (ac == 3)
 	{
+		pid = atoi(av[1]);
 		while (av[2][i] != '\0')
 		{
-			signal(SIGUSR1, show_mssg);
+			
 			signal(SIGUSR2, show_mssg);
+			signal(SIGUSR1, show_mssg);
 			transmit_mssg(pid, av[2][i]);
 			i++;
+			index++ ;
 		}
-		transmit_mssg(pid, '\n');
+		transmit_mssg(pid, '\0');
 	}
 	else
 		printf("***( ERROR -_-! )***\nTRY =>> ./client <server_pid> <text to send>\n");
--- server.c
+++ server.c
@@ -3,46 +3,106 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: younesmoukhlij <younesmoukhlij@student.    +#+  +:+       +#+        */
+/*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:54:51 by youmoukh          #+#    #+#             */
-/*   Updated: 2023/12/19 01:34:17 by younesmoukh      ###   ########.fr       */
+/*   Updated: 2023/12/21 21:33:47 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 
-static void	recieve_mssg(int signal_value)
+static void	recieve_mssg(int signal_value, siginfo_t *t, void *a)
 {
-    static int	index = 0;
-    static int	octet = 0;
-	static int mssg_env = 0;
+    static int	index;
+    static unsigned char	octet;
+	static int mssg_env;
+	static int client;
+	static int current;
 
-	if (signal_value == SIGUSR1)
+	(void)a;
+	(void)t;
+	if (!client)
+		client = t->si_pid;
+	current = t->si_pid;
+	if (client != current)
+	{
+		client = t->si_pid;
+		octet = 0;
+		index = 0;
+		mssg_env = 0;
+	}
+	if (signal_value == SIGUSR2)
 		octet = octet | (0x01 << index);
+	if (signal_value == SIGUSR1)
+		octet = octet | (0x00 << index);
 	index++;
 	mssg_env++;
 	if (index == 8)
 	{
 		printf("%c", octet);
 		if ((char)octet == '\0')
-			printf("%d messages recieved\n", mssg_env);
+			printf("\n%d messages recieved\n", mssg_env);
+	
 		index = 0x0;
 		octet = 0x0;
+		mssg_env = 0x0;
+	
+		
 	}
+	
+	//kill(client, SIGUSR2);
 }
+// static void	recieve_mssg(int signal_value)
+// {
+//     static int	index = 0;
+//     static int	octet = 0;
+// 	static int mssg_env = 0;
 
+// 	if (signal_value == SIGUSR1)
+// 		octet = octet | (0x01 << index);
+// 	index++;
+// 	mssg_env++;
+// 	if (index == 8)
+// 	{
+// 		printf("%c", octet);
+// 		if ((char)octet == '\0')
+// 			printf("%d messages recieved\n", mssg_env);
+// 		index = 0x0;
+// 		octet = 0x0;
+// 	}
+// }
+
+// int	main(void)
+// {
+// 	int pid = getpid();
+
+//     printf("My Server's PID : %d\n", pid);
+// 	printf("\033[90mWaiting for a message...\033[0m\n");
+//     while (1)
+// 	{
+//     	signal(SIGUSR1, recieve_mssg);
+//     	signal(SIGUSR2, recieve_mssg);
+// 		pause();
+// 	}
+//     return (0);
+// }
 int	main(void)
 {
 	int pid = getpid();
+	
+	struct sigaction sss;
 
+	sss.sa_sigaction = recieve_mssg;
+	sigemptyset(&sss.sa_mask);
+	sss.sa_flags =  SA_SIGINFO;
     printf("My Server's PID : %d\n", pid);
 	printf("\033[90mWaiting for a message...\033[0m\n");
     while (1)
 	{
-    	signal(SIGUSR1, recieve_mssg);
-    	signal(SIGUSR2, recieve_mssg);
+    	sigaction(SIGUSR1, &sss, NULL);
+    	sigaction(SIGUSR2, &sss, NULL);
 		pause();
 	}
     return (0);
